bed109eb59a8fb6403579b87b1a211bef8e9734e
Uenhe <119563256+Uenhe@users.noreply.github.com>
2023-01-06 10:00:21 +0800

    Delete scripts directory
    fixing

--- scripts/units/gnj.js
+++
@@ -1,4 +0,0 @@
-var gnj = extend(UnitType, "gnj", {})
-gnj.defaultController = prov(() => new BuilderAI())
-gnj.constructor = prov(() => extend(UnitTypes.poly.constructor.get().class, {}))
-exports.gnj = gnj
