f4afd6f339fd942517dc11c9c6647091638f7a93
Uenhe <119563256+Uenhe@users.noreply.github.com>
2023-08-22 10:18:38 +0800

    e

--- src/fire/FireLib.java
+++ src/fire/FireLib.java
@@ -6,6 +6,7 @@ import arc.graphics.g2d.TextureRegion;
 import mindustry.gen.Building;
 
 public class FireLib{
+
     public static Color color(String hex){
         return Color.valueOf(hex);
     }
