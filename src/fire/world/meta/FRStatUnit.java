176510aa822e13f777e948099e7b46f9b06a1cd1
KochiyaUenehaaa <1836596978@qq.com>
2025-07-14 15:20:22 +0800

    test

--- src/fire/world/meta/FRStatUnit.java
+++
@@ -1,9 +0,0 @@
-package fire.world.meta;
-
-import mindustry.world.meta.StatUnit;
-
-public class FRStatUnit{
-
-    public static final StatUnit
-    percentPerSec = new StatUnit("percentPerSec");
-}
